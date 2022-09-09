6202010befb46a14f01ca7724d39e78b280d5194
Ryan Kustaborder <rkusty23@gmail.com>
2022-09-08 23:37:05 -0400

    Base Layout

--- fotoshop-js-app/src/App.js
+++ fotoshop-js-app/src/App.js
@@ -1,5 +1,13 @@
 function App() {
-    return <div className="App">App</div>;
+    return (
+        <div className="App">
+            <div id="header"></div>
+            <div id="sidebar"></div>
+            <div id="display"></div>
+            <div id="details"></div>
+            <div id="footer"></div>
+        </div>
+    );
 }
 
 export default App;
